4c8901519785ef447c56a03f466c368373f532fb
Francesco Cristallo <francesco@olihargon.com>
2022-11-11 22:38:24 -0600

    Add project files.

---
+++ BlazorApp-empty-testkeyup/Client/Pages/Index.razor.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+
+namespace BlazorAppemptytestkeyup.Client.Pages;
+public partial class Index
+{
+   public List<string> Logs { get; set; } = new();
+   public void KeyEvent(KeyboardEventArgs e)
+   {
+      Logs.Clear();
+      Logs.Add(e.Key);
+      Logs.Add(e.Code);
+      Logs.Add(e.Type);
+   }
+}
+
